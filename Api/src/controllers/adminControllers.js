a9f57fb9a6f59a13a1c6ab21d6db820c0068fd47
MikaelaMonroy <mika_monroy00@hotmail.com>
2023-12-01 10:43:22 -0300

    interfaz menu para celulares

--- Api/src/controllers/adminControllers.js
+++ Api/src/controllers/adminControllers.js
@@ -1,36 +1,48 @@
 const { Users, Product } = require("../db");
 
-const getAllAdmins = async (id, name, precio, descripcion, UserId) => {
+const getAllAdmins = async (id, name, precio, descripcion, type, image, UserId) => {
   try {
+    // Obtener todos los administradores
     const admins = await Users.findAll({
       where: {
         role: "admin",
       },
     });
 
+    // Crear productos para cada administrador
     const adminsWithProducts = await Promise.all(
       admins.map(async (admin) => {
-        const [product, created] = await Product.findOrCreate({
-          where: { id, name },
-          defaults: {
-            id, // clave: valor
-            name,
-            precio,
-            descripcion,
-            UserId
-          },
-        });
+        try {
+          // Buscar o crear el producto para el administrador actual
+          const [product, created] = await Product.findOrCreate({
+            where: { id, name, UserId: admin.id }, // Ajustar la condición de búsqueda
+            defaults: {
+              id,
+              name,
+              precio,
+              descripcion,
+              type,
+              image,
+              UserId: admin.id,
+            },
+          });
 
-        return {
-          admin,
-          product,
-          created,
-        };
+          // Devolver información sobre el administrador y el producto
+          return {
+            admin,
+            product,
+            created,
+          };
+        } catch (productError) {
+          console.error("Error while creating product:", productError);
+          throw new Error("Error while creating product for admin");
+        }
       })
     );
 
     return adminsWithProducts;
-  } catch (error) {
+  } catch (adminError) {
+    console.error("Error while getting admins:", adminError);
     throw new Error("Error while getting admins");
   }
 };
--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Menu from './components/views/Menu/Menu'
+import NavBar from './components/inc/NavBar/NavBar';
 
 function App() {
 
     return (
-        <div className="container">
+        <div className="max-w-full h-screen  ">
+            <NavBar></NavBar>
             <Menu></Menu>
         </div>
     );
